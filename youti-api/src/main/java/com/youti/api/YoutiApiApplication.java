e7b6ee7be6b6c42c485899cfbb9b98e68dff0d24
zxm3811 <1398128393@qq.com>
2018-10-22 22:23:24 +0800

    init repository

---
+++ youti-api/src/main/java/com/youti/api/YoutiApiApplication.java
@@ -0,0 +1,12 @@
+package com.youti.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutiApiApplication.class, args);
+	}
+}

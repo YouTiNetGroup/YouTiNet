69d2cf07e74e247ad9329806a8143148592620bf
zxm3811 <1398128393@qq.com>
2018-11-10 13:49:15 +0800

    fix MvcConfig

--- youti-api/src/main/java/com/youti/api/config/MvcConfig.java
+++ youti-api/src/main/java/com/youti/api/config/MvcConfig.java
@@ -1,6 +1,12 @@
 package com.youti.api.config;
 
+import java.nio.charset.Charset;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -12,6 +18,19 @@ public class MvcConfig extends WebMvcConfigurationSupport{
     @Override
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(new ProcessInterceptor()).addPathPatterns("/**");
+    }
+    
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter(){
+    StringHttpMessageConverter converter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    return converter;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    super.configureMessageConverters(converters);
+
+    converters.add(responseBodyConverter());
     }
 
 }

4ef147000eea2dfd44191bc6f945a3123ac271b0
zxm3811 <1398128393@qq.com>
2018-12-16 13:48:04 +0800

    fix

--- youti/src/page/backstage/home.vue
+++ youti/src/page/backstage/home.vue
@@ -27,7 +27,6 @@ export default {
   .container {
     margin: 1rem 1rem 0 1rem;
     padding: 0.5rem 0.5rem 0 0.5rem;
-    border-top: 1px solid #ccc;
     display: flex;
     flex-direction: column;
     align-items: center;
